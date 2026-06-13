7e2a50a2810980bfd3c148e90fbe00c73cd0ad8c
CARTOPIA0319 <tadashi_10_23@yahoo.co.jp>
2026-06-13 23:32:59 +0900

    Update route.js

--- app/api/inventory/route.js
+++ app/api/inventory/route.js
@@ -1,6 +1,22 @@
 export async function GET() {
-  return Response.json({
-    status: "ok",
-    message: "inventory api ready"
-  });
+  return Response.json([
+    {
+      maker: "トヨタ",
+      name: "シエンタ",
+      grade: "ハイブリッドZ",
+      price: 198.8
+    },
+    {
+      maker: "ホンダ",
+      name: "フリード",
+      grade: "e:HEV AIR",
+      price: 189.8
+    },
+    {
+      maker: "日産",
+      name: "セレナ",
+      grade: "ハイウェイスターV",
+      price: 259.8
+    }
+  ]);
 }
