aff19206a1cd608d707668379f14e9673cc9d37b
CARTOPIA0319 <tadashi_10_23@yahoo.co.jp>
2026-07-19 22:48:21 +0900

    Add makers dictionary

---
+++ app/api/direct-input/dictionary/makers.js
@@ -0,0 +1,111 @@
+export const makers = [
+  {
+    name: "トヨタ",
+    keywords: ["トヨタ", "TOYOTA", "Toyota"]
+  },
+  {
+    name: "レクサス",
+    keywords: ["レクサス", "LEXUS", "Lexus"]
+  },
+  {
+    name: "日産",
+    keywords: ["日産", "ニッサン", "NISSAN", "Nissan"]
+  },
+  {
+    name: "ホンダ",
+    keywords: ["ホンダ", "HONDA", "Honda"]
+  },
+  {
+    name: "マツダ",
+    keywords: ["マツダ", "MAZDA", "Mazda"]
+  },
+  {
+    name: "スズキ",
+    keywords: ["スズキ", "SUZUKI", "Suzuki"]
+  },
+  {
+    name: "ダイハツ",
+    keywords: ["ダイハツ", "DAIHATSU", "Daihatsu"]
+  },
+  {
+    name: "三菱",
+    keywords: ["三菱", "ミツビシ", "MITSUBISHI", "Mitsubishi"]
+  },
+  {
+    name: "スバル",
+    keywords: ["スバル", "SUBARU", "Subaru"]
+  },
+  {
+    name: "いすゞ",
+    keywords: ["いすゞ", "ISUZU", "Isuzu"]
+  },
+  {
+    name: "日野",
+    keywords: ["日野", "HINO", "Hino"]
+  },
+  {
+    name: "UDトラックス",
+    keywords: ["UD", "UDトラックス"]
+  },
+
+  {
+    name: "メルセデス・ベンツ",
+    keywords: ["ベンツ", "Mercedes", "Mercedes-Benz", "メルセデス"]
+  },
+  {
+    name: "BMW",
+    keywords: ["BMW", "ビーエム"]
+  },
+  {
+    name: "アウディ",
+    keywords: ["アウディ", "Audi"]
+  },
+  {
+    name: "フォルクスワーゲン",
+    keywords: ["VW", "Volkswagen", "フォルクスワーゲン"]
+  },
+  {
+    name: "ポルシェ",
+    keywords: ["ポルシェ", "Porsche"]
+  },
+  {
+    name: "MINI",
+    keywords: ["MINI", "ミニ"]
+  },
+  {
+    name: "ボルボ",
+    keywords: ["ボルボ", "Volvo"]
+  },
+  {
+    name: "ランドローバー",
+    keywords: ["ランドローバー", "Land Rover"]
+  },
+  {
+    name: "ジープ",
+    keywords: ["ジープ", "Jeep"]
+  },
+  {
+    name: "テスラ",
+    keywords: ["テスラ", "Tesla"]
+  },
+  {
+    name: "フェラーリ",
+    keywords: ["フェラーリ", "Ferrari"]
+  },
+  {
+    name: "ランボルギーニ",
+    keywords: ["ランボルギーニ", "Lamborghini"]
+  },
+  {
+    name: "マセラティ",
+    keywords: ["マセラティ", "Maserati"]
+  },
+  {
+    name: "ベントレー",
+    keywords: ["ベントレー", "Bentley"]
+  },
+  {
+    name: "ロールス・ロイス",
+    keywords: ["ロールスロイス", "Rolls-Royce"]
+  }
+];
